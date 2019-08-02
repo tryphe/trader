e0bf35621d458164ada4421bacc8182a609d44b8
tryphe <kwentzell@gmail.com>
2019-08-02 01:59:40 -0400

    cli: avoid crash with empty call

--- cli/main.cpp
+++ cli/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     args.removeFirst(); // remove application name
 
     // form arguments for command caller
-    QString exchange = args.takeFirst();
+    QString exchange = args.size() == 0 ? QLatin1String() : args.takeFirst();
     QString joined = args.join( QChar( ' ' ) );
 
     CommandCaller *c = new CommandCaller( exchange, joined.toLocal8Bit() );
