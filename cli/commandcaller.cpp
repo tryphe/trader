35447c81986e2acd5b2cca81ef3866797e68179f
tryphe <kwentzell@gmail.com>
2019-08-08 09:41:46 -0400

    commandcaller: optimization

--- cli/commandcaller.cpp
+++ cli/commandcaller.cpp
@@ -10,17 +10,16 @@ CommandCaller::CommandCaller( QString exchange, QByteArray command, QObject *par
     exchange = exchange.toLower();
 
     // parse the first argument as the daemon target
-    QString subpath;
-    if ( exchange == "binance" ) subpath = BINANCE_SUBPATH;
-    else if ( exchange == "bittrex" ) subpath = BITTREX_SUBPATH;
-    else if ( exchange == "poloniex" ) subpath = POLONIEX_SUBPATH;
+    if ( exchange == "binance" ) exchange = BINANCE_SUBPATH;
+    else if ( exchange == "bittrex" ) exchange = BITTREX_SUBPATH;
+    else if ( exchange == "poloniex" ) exchange = POLONIEX_SUBPATH;
     else
     {
         qDebug() << "error: an incorrect exchange was given, use `trader-cli <bittrex|poloniex|binance> <command>`";
         return;
     }
 
-    QString path = Global::getIPCPath( subpath );
+    QString path = Global::getIPCPath( exchange );
     QLocalSocket::connectToServer( path );
     QLocalSocket::waitForConnected();
 
