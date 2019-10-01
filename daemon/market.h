0a04ed9d0cd9e2c0b4b0bd43d2af9be8598cc005
tryphe <kwentzell@gmail.com>
2019-10-01 01:40:48 -0400

    marketinfo: use init by declaration

--- daemon/market.h
+++ daemon/market.h
@@ -31,23 +31,6 @@ struct MarketInfo
 {
     explicit MarketInfo()
     {
-        order_min = 5;
-        order_max = 10;
-        order_dc = 1;
-        order_dc_nice = 0;
-        order_landmark_thresh = 0;
-        order_landmark_start = 0;
-        slippage_timeout = 2 * 60000;
-        market_offset = 0.;
-        market_sentiment = false;
-
-#if defined(EXCHANGE_BINANCE)
-        price_min_mul = 0.2;
-        price_max_mul = 5.0;
-
-        quantity_ticksize = CoinAmount::SATOSHI;
-#endif
-        price_ticksize = CoinAmount::SATOSHI;
     }
 
     operator QString() const { return QString( "bid %1 ask %2 order_min %3 order_max %4 order_dc %5 order_dc_nice %6 order_landmark_thresh %7 order_landmark_start %8 slippage_timeout %9 market_offset %10 market_sentiment %11" )
@@ -92,24 +75,25 @@ struct MarketInfo
 
     // ping-pong settings
     QVector<PositionData> /*position_index*/ position_index;
-    qint32 /*order count limit*/ order_min;
-    qint32 /*order count limit*/ order_max;
-    qint32 /*order count combine*/ order_dc;
-    qint32 /*nice value*/ order_dc_nice;
-    qint32 /*n*/ order_landmark_thresh;
-    qint32 /*n*/ order_landmark_start;
-    qint32 /*timeout secs*/ slippage_timeout;
-    qreal /*offset scalar*/ market_offset;
-    bool /*bullish*/ market_sentiment;
+    qint32 /*order count limit*/ order_min{ 5 };
+    qint32 /*order count limit*/ order_max{ 10 };
+    qint32 /*order count combine*/ order_dc{ 1 };
+    qint32 /*nice value*/ order_dc_nice{ 0 };
+    qint32 /*n*/ order_landmark_thresh{ 0 };
+    qint32 /*n*/ order_landmark_start{ 0 };
+    qint32 /*timeout secs*/ slippage_timeout{ 2 * 60000 };
+    qreal /*offset scalar*/ market_offset{ 0. };
+    bool /*bullish*/ market_sentiment{ false };
+
+    // per-market settings
+    Coin price_ticksize{ CoinAmount::SATOSHI }; // used to find new prices and pass binance filter PRICE_FILTER "tickSize"
 
 #if defined(EXCHANGE_BINANCE)
     // used to pass filter PERCENT_PRICE
-    Coin price_min_mul;
-    Coin price_max_mul;
-
-    Coin quantity_ticksize; // used to pass filter LOT_SIZE "stepSize"
+    Coin price_min_mul{ 0.2 };
+    Coin price_max_mul{ 5.0 };
+    Coin quantity_ticksize{ CoinAmount::SATOSHI }; // used to pass filter LOT_SIZE "stepSize"
 #endif
-    Coin price_ticksize; // used to find new prices and pass binance filter PRICE_FILTER "tickSize"
 };
 
 #endif // MARKET_H
