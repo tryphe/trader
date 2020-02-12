aa67971f9cbd82aa0927486c6d80561808f54898
tryphe <kwentzell@gmail.com>
2020-02-12 19:20:28 +0000

    alphatracker: remove fee from pl estimate

--- daemon/alphatracker.cpp
+++ daemon/alphatracker.cpp
@@ -103,7 +103,7 @@ void AlphaTracker::printAlpha() const
                     .arg( getTrades( market ), -7 );
 
         total_volume += volume;
-        estimated_pl += ( alpha - CoinAmount::COIN - ( Coin( BITTREX_DEFAULT_FEERATE ) *2 ) ) * volume;
+        estimated_pl += ( alpha - CoinAmount::COIN /*- ( Coin( BITTREX_DEFAULT_FEERATE ) *2 )*/ ) * volume;
     }
 
     kDebug() << "total volume:" << total_volume;
