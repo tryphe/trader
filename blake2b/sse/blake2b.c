56e2c0554bd551d34cff8abba866301da81b59da
tryphe <kwentzell@gmail.com>
2020-02-16 19:32:45 +0000

    blake2b: remove C99 int constants (pedantic fixup)

--- blake2b/sse/blake2b.c
+++ blake2b/sse/blake2b.c
@@ -43,10 +43,10 @@
 
 static const uint64_t blake2b_IV[8] =
 {
-  0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
-  0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
-  0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-  0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
+  0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
+  0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+  0x510e527fade682d1, 0x9b05688c2b3e6c1f,
+  0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
 };
 
 /* Some helper functions */
@@ -298,7 +298,7 @@ int blake2( void *out, size_t outlen, const void *in, size_t inlen, const void *
 }
 
 #if defined(SUPERCOP)
-int crypto_hash( unsigned char *out, unsigned char *in, unsigned long long inlen )
+int crypto_hash( unsigned char *out, unsigned char *in, uint64_t inlen )
 {
   return blake2b( out, BLAKE2B_OUTBYTES, in, inlen, NULL, 0 );
 }
