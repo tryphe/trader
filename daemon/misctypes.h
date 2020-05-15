718d961877683b54f28b2dbd5cd7737c20e8580b
tryphe <kwentzell@gmail.com>
2020-05-15 05:45:36 +0000

    misctypes: add in CoinAverage, CoinMovingAverage, CoinAverageTester

--- daemon/misctypes.h
+++ daemon/misctypes.h
@@ -5,14 +5,13 @@
 
 #include <QString>
 #include <QByteArray>
+#include <QQueue>
 
 class Position;
 
 struct Request
 {
-    explicit Request()
-    {
-    }
+    explicit Request() {}
 
     QString api_command;
     QString body;
@@ -42,9 +41,7 @@ struct OrderInfo
 
 struct TickerInfo
 {
-    explicit TickerInfo()
-    {
-    }
+    explicit TickerInfo() {}
     explicit TickerInfo( const Coin &_bid_price,
                          const Coin &_ask_price )
     {
@@ -66,9 +63,7 @@ struct TickerInfo
 class AvgResponseTime
 {
 public:
-    explicit AvgResponseTime()
-    {
-    }
+    explicit AvgResponseTime() {}
 
     void addResponseTime( quint64 time )
     {
@@ -80,6 +75,104 @@ public:
 private:
     quint64 total{ 0 },
             iterations{ 0 };
+};
+
+class CoinAverage
+{
+public:
+    explicit CoinAverage() {}
+
+    void addSample( const Coin &sample )
+    {
+        total += sample;
+        iterations++;
+    }
+    Coin getAverage() const { return iterations == 0 ? Coin() : total / iterations; }
+
+private:
+     Coin total;
+     quint64 iterations{ 0 };
+};
+
+class CoinMovingAverage
+{
+public:
+    CoinMovingAverage() {}
+    CoinMovingAverage( int _samples_max ) { setMaxSamples( _samples_max ); }
+
+    void setMaxSamples( int max ) { samples_max = max; }
+    int getMaxSamples() const { return samples_max; }
+    int getCurrentSamples() const { return samples.size(); }
+    void clear() { samples.clear(); }
+    void addSample( const Coin &sample )
+    {
+        samples.push_back( sample );
+        while ( samples_max > 0 && samples.size() > samples_max )
+            samples.removeFirst();
+    }
+    Coin getAverage() const
+    {
+        if ( samples.size() == 0 )
+            return Coin();
+
+        const int samples_size = samples.size();
+        Coin total;
+        for ( int i = 0; i < samples_size; i++ )
+            total += samples.value( i );
+
+        return total / samples_size;
+    }
+
+private:
+     QQueue<Coin> samples;
+     int samples_max{ 0 };
+};
+
+struct CoinAverageTester
+{
+    void test()
+    {
+        // test basic average
+        CoinAverage avg_test;
+        avg_test.addSample( Coin("1.5") );
+        avg_test.addSample( Coin("2.5") );
+        avg_test.addSample( Coin("3.5") );
+        avg_test.addSample( Coin("4.5") );
+        avg_test.addSample( Coin("5.5") );
+
+        assert( avg_test.getAverage() == Coin("3.5") );
+
+        // test moving average
+        CoinMovingAverage mavg_test( 10 );
+        mavg_test.addSample( Coin("1") );
+        mavg_test.addSample( Coin("2") );
+        mavg_test.addSample( Coin("3") );
+
+        assert( mavg_test.getAverage() == Coin("2") );
+
+        mavg_test.addSample( Coin("4") );
+        mavg_test.addSample( Coin("5") );
+        mavg_test.addSample( Coin("6") );
+
+        assert( mavg_test.getAverage() == Coin("3.5") );
+
+        mavg_test.addSample( Coin("7") );
+        mavg_test.addSample( Coin("8") );
+        mavg_test.addSample( Coin("9") );
+        mavg_test.addSample( Coin("10") );
+        mavg_test.addSample( Coin("11") );
+
+        assert( mavg_test.getAverage() == Coin("6.5") ); // note: 2:11 / 10 == 6.5
+
+        mavg_test.addSample( Coin("12") );
+        mavg_test.addSample( Coin("13") );
+
+        assert( mavg_test.getAverage() == Coin("8.5") ); // note: 4:13 / 10 == 8.5
+
+        mavg_test.clear();
+
+        assert( mavg_test.getCurrentSamples() == 0 );
+    }
 };
 
 #endif // MISCTYPES_H
