f3ad81b4433797b0d3079280ae241be3faf4cb4a
tryphe <kwentzell@gmail.com>
2020-05-25 02:31:05 +0000

    diffusionphaseman_test: extend test to verify per-phase markets

--- daemon/diffusionphaseman_test.cpp
+++ daemon/diffusionphaseman_test.cpp
@@ -3,6 +3,7 @@
 
 #include <QString>
 #include <QVector>
+#include <QMap>
 
 void DiffusionPhaseManTest::test()
 {
@@ -21,23 +22,40 @@ void DiffusionPhaseManTest::test()
     int sell_count = 0;
     int flux_count = 0;
     int noflux_count = 0;
+    QMap<QString,int> flux_markets_accounted_for;
 
     for ( pm.begin(); !pm.atEnd(); pm.next() )
     {
         const quint8 side = pm.getCurrentPhaseSide();
         const bool is_flux_phase = ( pm.getCurrentPhase() == FLUX_PER_MARKET );
 
+        // count iterations of different phase types
         if ( is_flux_phase )
             flux_count++;
         else
             noflux_count++;
 
+        // count phases per size
         if ( side == SIDE_BUY )
             buy_count++;
         else
             sell_count++;
+
+        // make sure the execution market matches the correct market count
+        if ( is_flux_phase )
+        {
+            const QString &current_active_market = pm.getCurrentPhaseMarkets().value( 0 );
+            flux_markets_accounted_for[ current_active_market ]++;
+
+            assert( pm.getCurrentPhaseMarkets().size() == 1 ); // make sure it's a single market
+            assert( ( side == SIDE_BUY  && flux_markets_accounted_for.value( current_active_market ) == 1 ) ||
+                    ( side == SIDE_SELL && flux_markets_accounted_for.value( current_active_market ) == 2 ) ); // ensure market is fluxed once on each side
+        }
+        else
+            assert( pm.getCurrentPhaseMarkets() == markets );
     }
 
+    assert( flux_markets_accounted_for.size() == markets.size() ); // verify we fluxed as many markets as supplied
     assert( flux_count == markets.size() *2 ); // one buy/sell flux phase for each market
     assert( noflux_count == 2 ); // noflux always has 1 buy/sell phase
     assert( buy_count == sell_count );
